8cd7056a16996574ddf50b82c548ddba554d4437
Sheryl Lun <sheryl@smugmug.com>
2023-03-10 14:19:12 -0800

    Sync with Pi updates

--- labelize.js
+++ labelize.js
@@ -1,10 +1,11 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
 
-AWS.config.loadFromPath('/home/pi/config.json');
+//AWS.config.loadFromPath('./config.json');
+ AWS.config.loadFromPath('/home/pi/smartvision/config.json');
 
-let rekognition = new AWS.Rekognition({ apiVersion: '2016-06-27' });
 let file = process.argv[2];
+let rekognition = new AWS.Rekognition({ apiVersion: "2016-06-27" });
 
 // pull base64 representation of image from file system
 fs.readFile(file, 'base64', (err, data) => {
@@ -13,34 +14,48 @@ fs.readFile(file, 'base64', (err, data) => {
     const buffer = new Buffer(data, 'base64');
 
     // now that we have things in the right type, send it to rekognition
-    rekognition.detectLabels({
+    rekognition
+      .detectLabels({
         Image: {
-            Bytes: buffer
+          Bytes: buffer,
         },
         MaxLabels: 15,
-        MinConfidence: 75
-    }).promise()
-    .then((res) => {
+        MinConfidence: 86,
+      })
+      .promise()
+      .then((res) => {
         let matchResults = parseResults(res.Labels);
         if (matchResults.length) {
-            // Return the results as a string to bash script to send email
-            // WBN to pass this directly to the node email script, but
-            // the Google OAuth2 Gmail client is CLI so I couldn't pass the results
-            // through as params
-            console.log(matchResults.join(', '));
+          // return the results as a string to bash script to send email
+          console.log(matchResults.join(", "));
         } else {
-            console.log('No match');
+          console.log("No match");
         }
-    });
+      });
 });
 
 function parseResults(results) {
-    let searchTerms = ['Human', 'Person', 'People', 'Male', 'Female', 'Selfie', 'Costume', 'Animal', 'Mammal', 'Cat', 'Pet', 'Dog'];
-    let foundTerms = [];
-    results.map(result => {
-        if (searchTerms.indexOf(result.Name) >= 0) {
-            foundTerms.push(result.Name);
-        }
-    });
-    return foundTerms;
+  let searchTerms = [
+    "Human",
+    "Person",
+    "People",
+    "Male",
+    "Female",
+    "Selfie",
+    "Costume",
+    "Animal",
+    "Mammal",
+    "Cat",
+    "Pet",
+    "Dog",
+  ];
+  let foundTerms = [];
+  results.map((result) => {
+    if (searchTerms.indexOf(result.Name) >= 0) {
+      let confidence = result.Confidence.toFixed(2);
+      let label = `${result.Name} ${confidence}%`;
+      foundTerms.push(label);
+    }
+  });
+  return foundTerms;
 }
